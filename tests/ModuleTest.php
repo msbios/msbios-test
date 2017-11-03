05964924c461b8979c98e45c75a93610452bdfc7
Judzhin Miles <info@msbios.com>
2017-11-03 17:44:15 +0200

    Lazy Composer Commiter

---
+++ tests/ModuleTest.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @access protected
+ * @author Judzhin Miles <info[woof-woof]msbios.com>
+ */
+namespace MSBiosTest\Test;
+
+use MSBios\Test\Module;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class ModuleTest
+ * @package MSBiosTest\Test
+ */
+class ModuleTest extends TestCase
+{
+    /**
+     *
+     */
+    public function testGetConfig()
+    {
+        $this->assertInternalType('array', (new Module)->getConfig());
+    }
+
+    /**
+     *
+     */
+    public function testGetAutoloaderConfig()
+    {
+        $this->assertInternalType('array', (new Module)->getAutoloaderConfig());
+    }
+}
