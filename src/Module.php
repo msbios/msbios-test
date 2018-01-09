3aa898b96b369a011c10116f6c04e91b6df18140
Judzhin Miles <info@msbios.com>
2018-01-09 21:39:50 +0200

    Lazy Composer Commiter

--- src/Module.php
+++ src/Module.php
@@ -12,12 +12,12 @@ use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 
 /**
  * Class Module
- * @package MSBios\Application
+ * @package MSBios\Test
  */
 class Module implements ModuleInterface, AutoloaderProviderInterface
 {
     /** @const VERSION */
-    const VERSION = '1.0.3';
+    const VERSION = '1.0.4';
 
     /**
      * @return mixed
