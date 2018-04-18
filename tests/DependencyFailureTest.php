7d31cf53a1262b8dfbeb72f534745f2927175998
Judzhin Miles <info@msbios.com>
2018-04-18 10:02:35 +0300

    Lazy Composer Commiter

--- tests/DependencyFailureTest.php
+++ tests/DependencyFailureTest.php
@@ -15,12 +15,13 @@ class DependencyFailureTest extends TestCase
 {
     public function testOne()
     {
-        $this->assertTrue(true);
+        $this->assertTrue(false);
     }
     /**
      * @depends testOne
      */
     public function testTwo()
     {
+
     }
 }
---
+++ tests/MultipleDependenciesTest.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @access protected
+ * @author Judzhin Miles <info[woof-woof]msbios.com>
+ */
+namespace MSBiosTest\Test;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ *
+ */
+class MultipleDependenciesTest extends TestCase
+{
+    public function testProducerFirst()
+    {
+        $this->assertTrue(true);
+        return 'first';
+    }
+
+    public function testProducerSecond()
+    {
+        $this->assertTrue(true);
+        return 'second';
+    }
+
+    /**
+     * @depends testProducerFirst
+     * @depends testProducerSecond
+     */
+    public function testConsumer()
+    {
+        $this->assertEquals(
+            ['first', 'second'],
+            func_get_args()
+        );
+    }
+}
