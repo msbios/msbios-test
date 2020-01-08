1c5efd67b141f033d07e66b3c3a7d32b9aa753bd
Judzhin Miles <info@msbios.com>
2020-01-08 03:50:21 +0200

    Lazy Composer MSBios Commit Script

--- src/Module.php
+++ src/Module.php
@@ -5,39 +5,14 @@
  */
 namespace MSBios\Test;
 
-use MSBios\ModuleInterface;
-use Zend\Loader\AutoloaderFactory;
-use Zend\Loader\StandardAutoloader;
-use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
-
 /**
  * Class Module
  * @package MSBios\Test
  * @link https://phpunit.de/manual/6.5/en/phpunit-book.pdf
  * @page 47
  */
-class Module extends \MSBios\Module
+class Module
 {
     /** @const VERSION */
-    const VERSION = '1.0.10';
-
-    /**
-     * @inheritdoc
-     *
-     * @return string
-     */
-    protected function getDir()
-    {
-        return __DIR__;
-    }
-
-    /**
-     * @inheritdoc
-     *
-     * @return string
-     */
-    protected function getNamespace()
-    {
-        return __NAMESPACE__;
-    }
+    const VERSION = '2.0.0';
 }
